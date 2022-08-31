3a90a1c18e046973ec128824f111e8fb4e503b04
Julius Caezar <julius.caezar25@gmail.com>
2022-08-31 12:01:08 -0400

    new function

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+import './styles/index.css'
+
+function Card() {
+
+  return (
+    <div>
+      <strong>Julius Caezar</strong>
+      <small>10:15:23</small>
+    </div>
+  )
+}
+
+export default Card
